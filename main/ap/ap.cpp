0457e57794b775342d968d6e17f9b4404a0f1b95
Hancheol Cho <chcbaram@gmail.com>
2022-09-13 23:15:09 +0900

    코드정리 및 부팅 로고 이미지 변경

--- main/ap/ap.cpp
+++ main/ap/ap.cpp
@@ -12,48 +12,11 @@
 
 
 
-LVGL_IMG_DEF(test_img);
 
 
 void apInit(void)
 {
   cliOpen(_DEF_UART2, 115200);
-
-  image_t logo;
-  
-  logo = lcdCreateImage(&test_img, 0, 0, 0, 0);
-
-  uint32_t pre_time;
-  uint32_t exe_time;
-
-  pre_time = micros();
-  lcdClearBuffer(black);
-  lcdDrawImage(&logo, 0, 0);
-  exe_time = micros()-pre_time;
-  lcdPrintf(0, 0, red, "%d us", exe_time);
-
-
-  pre_time = micros();
-  static uint8_t *p_test;
-
-  p_test = (uint8_t *)heap_caps_malloc(LCD_WIDTH * LCD_HEIGHT * sizeof(uint16_t), MALLOC_CAP_SPIRAM);
-
-  if (p_test != NULL)
-  {
-    for (int i=0; i<LCD_WIDTH*LCD_HEIGHT*2; i++)
-    {
-      p_test[i] = 0;
-    }
-  }
-  else
-  {
-    logPrintf("p_test NULL\n");
-  }
-  exe_time = micros()-pre_time;
-  
-  lcdPrintf(0, 32, red, "%d us", exe_time);
-
-  lcdUpdateDraw();  
 }
 
 void apMain(void)
